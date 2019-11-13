4e7d84723fb011b2a9708cf8fcf135d32830e6cd
fuzhiqun <1254446598@qq.com>
2019-11-13 22:25:23 +0800

    little change

--- miao/lodash/sarah125444-lodash.js
+++ miao/lodash/sarah125444-lodash.js
@@ -1071,7 +1071,7 @@ var sarah125444 = {
   },
 
   max: function(array) {
-    return array.length === 0 ? undefined : array.sort((a, b) => a - b)[0];
+    return array.length === 0 ? undefined : array.sort((a, b) => b - a)[0];
   },
 
   maxBy: function(array, iteratee) {
